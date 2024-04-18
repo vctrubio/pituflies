65a1e5566fe4093585a92b1f4fbfda2e5b763be5
vctrubio <vctrubio@gmail.com>
2024-04-18 11:21:19 +0200

    banner set up

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import { Questionary } from './questionary'
 import { NavbarMini } from './navbarmini'
 import { NavbarMain } from './navbarmain'
+import { Banner } from './banner'
 function App() {
   return (
     <>
       <div id="app">
         <NavbarMini />
         <NavbarMain />
+        <Banner />
       </div>
     </>
   )
---
+++ src/banner.jsx
@@ -0,0 +1,10 @@
+
+export const Banner = () => {
+    return (
+        <div className="banner">
+            <div className="banner-text">
+                Aqui Va Text Ladies
+            </div>
+        </div>
+    )
+}
